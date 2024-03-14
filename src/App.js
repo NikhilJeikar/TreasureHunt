c3d79507f99f691e44ead1f26cd226d5897d532d
Nikhil Jeikar P M (njeikarp) <njeikarp@cisco.com>
2024-03-15 04:23:50 +0530

    Added table

--- src/App.js
+++ src/App.js
@@ -4,28 +4,49 @@ import { useEffect, useState } from "react";
 function App() {
   const queryParameters = new URLSearchParams(window.location.search);
   const id = queryParameters.get("id");
-  
-  const [msg, setMsg] = useState("We are CISCO");
-  
-  const path = localStorage.getItem("path")
 
-  useEffect(()=>{
+  const [msg, setMsg] = useState("We are CISCO");
+
+  let path = JSON.parse(localStorage.getItem("path"));
+  let timing = JSON.parse(localStorage.getItem("timing"));
+
+  useEffect(() => {
     fetch(`/codechecker/report/${id}`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-      body: {path:path}
+      body: { path: path },
     }).then((res) => {
       const resp = res.json();
-      setMsg(resp.clue)
-    })
-  })
-  
+      setMsg(resp.clue);
+      if (resp.status) {
+        path.push(id);
+        localStorage.setItem("path", path);
+        timing.push({
+          date: new Date().toLocaleDateString(),
+          time: new Date().toLocaleTimeString(),
+        });
+        localStorage.setItem("timing", timing);
+      }
+    });
+  });
+
   return (
     <div className="App">
-      {msg}
+      <div>{msg}</div>
+      <div>
+        {msg === "You have completed"
+          ? timing.map((value, ind) => {
+              return (
+                <div>
+                  <div>{`${ind} found at ${value.date} ${value.time}`}</div>
+                </div>
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 }
